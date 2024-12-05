8c44be100f841eef02e3283fc892906aa3e3b16f
D0ssisme <dossdoss29052005@gmail.com>
2024-12-05 15:01:33 +0700

    reset nộp bài

--- assets/js/checkCart.js
+++
@@ -1,8 +0,0 @@
-const cartLink = document.getElementById('cartIcon');
-cartLink.addEventListener('click', function (event) {
-    if (localStorage.getItem('isLoggedIn') !== 'true') {
-        event.preventDefault(); // Ngăn không cho liên kết hoạt động
-       
-        openModal();
-    }
-});
